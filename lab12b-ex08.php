8b5f4a7c1360b953decade67d5740b2493ebb158
Randy Connolly <rconnolly@mtroyal.ca>
2020-11-04 11:49:07 -0700

    some fixes

--- lab12b-ex08.php
+++ lab12b-ex08.php
@@ -35,7 +35,7 @@ $isbn = $defaultISBN;
             <?php
               foreach ($books as $key => $value) {
                   echo '<li>'; 
-                  echo '<a href="lab12-ex08.php">';
+                  echo '<a href="lab12b-ex08.php">';
                   echo $value['title'];
                   echo '</a>';
                   echo '</li>';      
